6dd3ce9257fd27524e06cc359481bf5bd4021e53
Marcin Wićko <mwicko@bluesoft.net.pl>
2014-02-03 06:16:14 +0100

    automatyczny druk etykiet

--- zlecenia_add/etykieta.php
+++ zlecenia_add/etykieta.php
@@ -1,21 +1,21 @@
 <?php
 
-require_once('../inc/fpdf/fpdf.php');
+require_once('../inc/fpdf/pdf_ap.php');
 define ( 'FPDF_FONTPATH', '../inc/fpdf/font/' );
 
-class EtykietaPDF extends FPDF
+class EtykietaPDF extends PDF_AutoPrint
 {
 
 	public $etykieta, $kategoria, $data, $termin, $barcode;
-
+		   
 	private $family = 'Times';
 	private $height = 5;
 	private $font_size = 12;
-
+	
 	function n() {
 		$this->SetFont($this->family, '', $this->font_size);
 	}
-
+	
 	function b() {
 		$this->SetFont($this->family, 'B', $this->font_size);
 	}
@@ -23,11 +23,8 @@ class EtykietaPDF extends FPDF
 	function i() {
 		$this->SetFont($this->family, 'I', $this->font_size);
 	}
-
+	
 	function rysuj_etykiete() {
-        $this->AddPage();
-        $this->SetXY(0,0);
-
 		$this->n();
 		$this->Cell(0,$this->height,$this->etykieta,0,1);
         $this->Ln();
@@ -35,34 +32,34 @@ class EtykietaPDF extends FPDF
 		$this->Cell(35, $this->height, $this->kategoria, 0, 0);
         $this->Ln();
 
-		$this->Cell(26, $this->height, 'wprowadzono: ', 0, 0);
+		$this->Cell(10, $this->height, 'wprowadzono: ', 0, 0);
 		$this->Cell(0, $this->height, $this->data, 0);
 		$this->Ln();
 
-        $this->Cell(30, $this->height, 'termin realizacji: ', 0, 0);
+        $this->Cell(25, $this->height, 'termin realizacji: ', 0, 0);
         $this->Cell(0, $this->height, $this->termin, 0);
         $this->Ln();
 
-        $this->Image($this->barcode, $this->GetX(), $this->GetY(), 0, 0, 'PNG');
-
+		$this->Cell(150, $this->height, $this->Image($this->barcode,0,0), 0, 0);
 		$this->Ln();
 	}
 
 	function draw($filename)
 	{
 		global $registry;
-
+		
 		$this->Open();
 		$this->author = "Us³ugi Protetyczne Andryskowski Jerzy";
-
+			
 		$this->AddFont('Georgia', '', 'georgia.php');
 		$this->AddFont('Times', '', 'times.php');
 		$this->AddFont('Times', 'B', 'timesbd.php');
 		$this->AddFont('Times', 'I', 'timesi.php');
 		$this->AddFont('Times', 'BI', 'timesbi.php');
-
+		
 		$this->SetMargins(0, 0, 0);
 		$this->rysuj_etykiete();
+        $this->AutoPrint(true);
 		$this->Output($filename);
 	}
 
